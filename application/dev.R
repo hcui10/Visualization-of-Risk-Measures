5da83e788098a1612651b924a9d1a65aa148a2b5
AkimotoAceAkira <hcui10@illinois.edu>
2018-01-01 04:20:03 -0600

    modified dev.R

--- application/dev.R
+++ application/dev.R
@@ -3,20 +3,29 @@ library("plotly")  # interavtie plot functions
 library("zeallot") # unpacking assignment
 source("helpers.R")
 
+# hard-coded color palette
+color.list <- c("#1f77b4", # muted blue
+                "#9467bd", # muted purple
+                "#2ca02c") # cooked asparagus green
+
+n.points <- 10000 # number of points to sample for x and p grids, hard-coded
+
 # check all distributions in the stats package
 # ?stats::distribution
-distribution <- "Normal"
-distribution.params <- list(mu = 0, sigma = 1)
-n.points <- 10000 # number of points to sample for x and p grids, hard-coded
+distribution <- "norm"
+distribution.params <- list(mean = 0, sd = 1)
 
 # generate data used for plots
 c(ddist, pdist, qdist) %<-% dist.helper(distribution, distribution.params)
-plot.data <- generate.data(distribution, distribution.params) 
+plot.data <- generate.data(distribution, distribution.params, n.points) 
 
+# plot without shading and spike lines
 plotly_plot(plot.data)
 
+# assume this is the click data
 c(x0, p0) %<-% list(x = 2, p = pdist(2))
 
+# plot upon click
 subplot(
   subplot(
     # PDF Plot ---
@@ -24,8 +33,7 @@ subplot(
             hoverinfo = "text", 
             text = ~paste("X: ", round.num(x, digits = 2), 
                           "<br> Density: ", round.num(fx, digits = 2)), 
-            # muted blue
-            color = I("#1f77b4")) %>% 
+            color = I(color.list[1])) %>% 
       add_lines(x = ~(x * (x <= x0)), 
                 y = ~(fx * (x <= x0)), 
                 fill = "tozeroy", name = "area under PDF") %>% 
@@ -39,8 +47,7 @@ subplot(
             text = ~paste("X: ", round.num(x, digits = 2), 
                           "<br> Cumulative Probability: ", 
                           round.num(Fx, digits = 2, type = "percent")), 
-            # muted purple
-            color = I("#9467bd")) %>% 
+            color = I(color.list[2])) %>% 
       add_lines() %>% 
       layout(xaxis = list(title = "$x$"), yaxis = list(title = "$p = F_X(x)$")), 
     # Rotated VaR Plot ---
@@ -49,8 +56,7 @@ subplot(
             text = ~paste("Value-at-Risk: ", round.num(VaR, digits = 2), 
                           "<br> Confidence Level: ", 
                           round.num(p, digits = 2, type = "percent")), 
-            # cooked asparagus green
-            color = I("#2ca02c")) %>% 
+            color = I(color.list[3])) %>% 
       add_lines() %>% 
       layout(xaxis = list(title = "$\\text{VaR}_X(p)$", autorange = "reversed")), 
     nrows = 2, shareX = TRUE, shareY = TRUE, which_layout = 3
@@ -60,8 +66,7 @@ subplot(
           hoverinfo = "text", 
           text = ~paste("Confidence Level: ", round.num(p, digits = 2, type = "percent"), 
                         "<br> Value-at-Risk: ", round.num(VaR, digits = 2)), 
-          # cooked asparagus green
-          color = I("#2ca02c")) %>% 
+          color = I(color.list[3])) %>% 
     add_lines() %>% 
     layout(xaxis = list(title = "$p$"), yaxis = list(title = "$\\text{VaR}_X(p)$")), 
   widths = c(0.7, 0.3), titleX = TRUE, titleY = TRUE
